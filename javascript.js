7ffca4dd890b9746ca751a6cf289b9b988cae066
shiri castro <shiricastro3@gmail.com>
2017-08-13 15:16:56 +0300

    add home page and imgs

--- javascript.js
+++ javascript.js
@@ -4,3 +4,18 @@ $('.flowerNav').on('click',function(e){
 	$('nav').toggleClass('activTagNav');
 	
 });
+
+var myIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var imgSlide = $(".mySlides");
+    imgSlide.each(function(i,el){
+    	$(el).css({"display": "none"}); 
+    });
+    myIndex++;
+    if (myIndex > imgSlide.length) {myIndex = 1}    
+    $(imgSlide[myIndex-1]).css({"display": "block"});   
+    setTimeout(carousel, 3000); // Change image every 2 seconds
+}
